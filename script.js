482fbcaf6efc383f7438fe331d5318261a80f5d4
amandaae <149404889+amandaae@users.noreply.github.com>
2024-08-09 08:48:42 -0300

    a

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 });
+
+<main>
+        <section id="contato">
+            <h1>Contato</h1>
+            
+        </section>
+        <section id="favoritos">
+            <h1>Favoritos</h1>
+            
+        </section>
+        <section id="carrinho">
+            <h1>Carrinho</h1>
+            
+        </section>
+    </main>
